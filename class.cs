88ce634adcef3fee0956203ea9c222900a497c22
Abhinash-kml <74886589+Abhinash-kml@users.noreply.github.com>
2026-08-12 16:34:11 +0530

    Tried out private backing field and manal property getter and setter with validation check

--- class.cs
+++ class.cs
@@ -1,11 +1,19 @@
+using System.ComponentModel.DataAnnotations;
 using System.Diagnostics.Contracts;
 using System.IO;
+
+// Main section
+Console.WriteLine("hello World");
+Entity ent = new Entity();
+ent.Age = 99;
+ent.Name = "neo";
+Console.WriteLine($"Entity info - \nId = {ent.Id}\nName = {ent.Name}\nAge = {ent.Age}");
 
 public class Entity
 {
     public Guid Id { get; init; } = Guid.CreateVersion7();
-    private string _name;
-    public string Name
+    private string? _name;
+    public string? Name
     {
         get
         {
@@ -24,14 +32,9 @@ public class Entity
         set
         {
             if ((value < 10) || (value > 100))
-                throw NullReferenceException;
+                throw new ArgumentException("The provided argument cannot pass validation");
 
             _age = value;
         }
-    }
-
-    
+    } 
 }
-
-// Main section
-Console.WriteLine("hello World");
