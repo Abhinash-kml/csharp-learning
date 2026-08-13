8701654e00a09b77e3ad963c1a3cb3767c1f5145
Abhinash-kml <74886589+Abhinash-kml@users.noreply.github.com>
2026-08-13 14:34:55 +0530

    Tried out inheritance, readonly property and field

--- class.cs
+++ class.cs
@@ -1,19 +1,30 @@
 using System.ComponentModel.DataAnnotations;
 using System.Diagnostics.Contracts;
 using System.IO;
+using System.Security.Cryptography;
 
 // Main section
 Console.WriteLine("hello World");
 Entity ent = new Entity();
 ent.Age = 99;
 ent.Name = "neo";
-Console.WriteLine($"Entity info - \nId = {ent.Id}\nName = {ent.Name}\nAge = {ent.Age}");
+ent.PrintDetails();
 
+Entity ent2 = new Entity("neo2", 55);
+ent2.PrintDetails();
+
+Human h = new Human("Human", 32);
+h.Hobbies?.Add("Singing");
+h.Hobbies?.AddRange(new List<string>{"Dancing", "Swimming"});
+int? len = h.Hobbies?.Count;
+Console.WriteLine("$Human has {len} hobbies");
+foreach (var hobby in h.Hobbies)
+    Console.WriteLine(hobby);
 public class Entity
 {
     public Guid Id { get; init; } = Guid.CreateVersion7();
-    private string? _name;
-    public string? Name
+    private string _name;
+    public string Name
     {
         get
         {
@@ -36,5 +47,32 @@ public class Entity
 
             _age = value;
         }
+    }
+
+    public Entity() {}
+    public Entity(string name, int age)
+    {
+        _name = name;
+        _age = age;
+
+        Console.WriteLine("Constructed");
+    }
+
+    public void PrintDetails()
+    {
+        Console.WriteLine($"Entity info - \nId = {Id}\nName = {Name}\nAge = {Age}");
     } 
+}
+
+public class Human : Entity
+{
+    public readonly string tag = "Human";
+    public int Health { get; } = 10;
+    public Human(string name, int age)
+        : base(name, age)
+        {}
+
+    public Human() {}
+
+    public List<string>? Hobbies { get; }
 }
