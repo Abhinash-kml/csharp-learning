533347018a431044502f10dfb4855ec97cad78aa
Abhinash-kml <74886589+Abhinash-kml@users.noreply.github.com>
2026-08-15 14:22:43 +0530

    Tried out struct and readonly struct

---
+++ struct.cs
@@ -0,0 +1,34 @@
+using System.IO;
+
+Point p = new Point(10.5f, 20.5f);
+Console.WriteLine($"Point P - {p.X} - {p.Y}");
+Point q = default;
+Console.WriteLine($"Point Q - {q.X} - {q.Y}");
+Student s = new Student();
+// s.Name = "Haha";
+
+
+internal struct Point
+{
+    public float X { get; set; }
+    public float Y { get; set; }
+
+    public Point(float x, float y)
+    {
+        X = x;
+        Y = y;
+    }
+}
+
+internal readonly struct Student
+{
+    public string Name { get; } = "Student 1";
+    public string Age { get; init; } = "20";
+
+    public Student() {}
+    public Student(string name, string age)
+    {
+        Name = name;
+        Age = age;
+    }
+}
