6a4485780f394cc30a9d52e3f3ae9897dcf5a4ed
Abhinash-kml <74886589+Abhinash-kml@users.noreply.github.com>
2026-08-17 12:24:11 +0530

    Tried out Struct generics

--- generics.cs
+++ generics.cs
@@ -2,6 +2,11 @@ using System.IO;
 
 Wrapper<int> w = new Wrapper<int>(109);
 w.Describe();
+var ww = new Wrapper<float>(100.0f);
+ww.Describe();
+GenericStruct<int> gs = new GenericStruct<int>();
+gs.Data = 100;
+gs.Describe();
 public class Wrapper<T>
 {
     public T Data { get; set; }
@@ -11,6 +16,15 @@ public class Wrapper<T>
         Data = data;
     }
 
+    public void Describe()
+    {
+        Console.WriteLine($"The internal data type is {typeof(T)}");
+    }
+}
+
+public struct GenericStruct<T>
+{
+    public T Data { get; set; }
     public void Describe()
     {
         Console.WriteLine($"The internal data type is {typeof(T)}");
