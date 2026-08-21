a4363946f26dca1eab6b74ee6a95bc8858ee2e2e
Abhinash-kml <74886589+Abhinash-kml@users.noreply.github.com>
2026-08-21 20:33:21 +0530

    Tried enum parsing and iterating over

--- enum.cs
+++ enum.cs
@@ -8,6 +8,14 @@ EWithType Default = EWithType.Default;
 EWithType Open = EWithType.Open;
 EWithType Closed = EWithType.Closed;
 
+foreach (var values in Enum.GetValues<EWithType>())
+    Console.WriteLine(values);
+
+var parsed = Enum.Parse<EWithType>("Default");
+Console.WriteLine($"Parsed enum = {parsed}");
+// parsed = Enum.Parse<EWithType>("Meow");
+// Console.WriteLine(parsed);
+
 enum State
 {
     Default,
