8cf4f0e72390ba33ab10907e1a86f2ddc6488907
Abhinash-kml <74886589+Abhinash-kml@users.noreply.github.com>
2026-08-22 14:41:11 +0530

    Tried out Action and Func delegate types

--- lambda.cs
+++ lambda.cs
@@ -7,5 +7,18 @@ b(10);
 
 CallbackWithReturn aa = (val) =>  { return val; };
 Console.WriteLine($"CallbackWithReturn = {aa(11)}");
+
+// Action is a delegate with no return type, basically void return type
+Action<int> aaa = (int value) => Console.WriteLine(value);
+aaa.Invoke(100);
+aaa(1000);
+
+// Func is a delegate with a return type other than void, the last type parameter is the return type
+Func<int, int> bbb = (int value) => value;
+var value = bbb.Invoke(200);
+Console.WriteLine(value);
+value = bbb(2000);
+Console.WriteLine(value);
+
 delegate void callback(int value);
 delegate int CallbackWithReturn(int value);
