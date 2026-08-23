bc593427f04c83a565e450949ee731867c616880
Abhinash-kml <74886589+Abhinash-kml@users.noreply.github.com>
2026-08-24 01:09:43 +0530

    Tried out null coallescing operator

--- null.cs
+++ null.cs
@@ -11,3 +11,8 @@ else
 // Unary Null conditional operator = checks if left operand is not null
 // then only executes right operand or it doesnt
 Console.WriteLine(a?.ToString());
+
+// Binary null coallescing operator
+// Evaluates left operand and returns its value if its not null else returns right one
+int b = a ?? 100;
+Console.WriteLine(b);
