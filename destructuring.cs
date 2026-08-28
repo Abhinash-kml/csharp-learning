9c0bc9bcaa48839c8b3ebe61ba20d7f1dd28639d
Abhinash-kml <74886589+Abhinash-kml@users.noreply.github.com>
2026-08-28 06:12:02 +0530

    Tried destructuring user defined type with Deconstruct method and out parameters

--- destructuring.cs
+++ destructuring.cs
@@ -1,15 +1,33 @@
 using System;
 using System.IO;
 
-var (a, b, c) = Entity.GetData();
+Entity Entity = new();
+
+var (a, b, c) = Entity.GetData(10, 10.0f, "10");
 Console.WriteLine($"{a}, {b}, {c}");
 
-(int aa, float bb, string cc) = Entity.GetData();
+(int aa, float bb, string cc) = Entity.GetData(10, 10.0f, "10");
 Console.WriteLine($"{aa}, {bb}, {cc}");
-public static class Entity
+
+(int aaa, var bbb, _) = Entity.GetData(20, 20.0f, "20");
+Console.WriteLine($"{aaa}, {bbb}");
+
+var (aaaa, bbbb, cccc) = Entity;
+Console.WriteLine($"{aaaa}, {bbbb}, {cccc}");
+public class Entity
 {
-    public static (int, float, string) GetData()
+    private int _a = 100;
+    private float _b = 100.0f;
+    private string _c = "100";
+    public static (int, float, string) GetData(int a, float b, string c)
     {
-        return (10, 10.0f, "10");
+        return (a ,b, c);
+    }
+
+    public void Deconstruct(out int a, out float b, out string c)
+    {
+        a = _a;
+        b = _b;
+        c = _c;
     }
 }
