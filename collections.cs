22a8633c96582cd3b2e280755b378c05a2dec6d3
Abhinash-kml <74886589+Abhinash-kml@users.noreply.github.com>
2026-08-31 14:23:11 +0530

    Tried out List collection

---
+++ collections.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+List<int> l1 = [1, 2, 3, 4, 5];
+Console.WriteLine(string.Join(", ", l1));
+l1.AddRange([6, 7, 8, 9, 10]);
+Console.WriteLine(string.Join(", ", l1));
+bool exists = l1.Contains(10);
+Console.WriteLine($"{exists}");
+exists = l1.Contains(100);
+Console.WriteLine($"{exists}");
+exists = l1.Exists((x) => x % 11 == 0);
+Console.WriteLine($"{exists}");
+l1.Add(11);
+Console.WriteLine(string.Join(", ", l1));
+l1.Remove(11);
+l1.RemoveAll(x => x % 2 == 0);
+Console.WriteLine(string.Join(", ", l1));
+l1.ForEach(x => x += 6);
+Console.WriteLine(string.Join(", ", l1));
+Dictionary<int, int> d = l1.ToDictionary<int, int>(x => x);
+foreach (var (key, value) in d)
+    Console.WriteLine($"{key}-{value}");
