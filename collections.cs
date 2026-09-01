3e4e0ad8bda88540bf8e98f3277d0d8f2610026a
Abhinash-kml <74886589+Abhinash-kml@users.noreply.github.com>
2026-09-01 08:55:01 +0530

    Tried out Dictionary

--- collections.cs
+++ collections.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
+// List -- dynamic array
 List<int> l1 = [1, 2, 3, 4, 5];
 Console.WriteLine(string.Join(", ", l1));
 l1.AddRange([6, 7, 8, 9, 10]);
@@ -23,3 +24,23 @@ Console.WriteLine(string.Join(", ", l1));
 Dictionary<int, int> d = l1.ToDictionary<int, int>(x => x);
 foreach (var (key, value) in d)
     Console.WriteLine($"{key}-{value}");
+
+Console.WriteLine("---------------------------------------");
+
+// Dictionary
+Dictionary<int, string> d1 = new();
+d1[1] = "1";
+d1.Add(2, "2");
+if (d1.TryAdd(3, "3"))
+    Console.WriteLine("Added");
+
+foreach (var (key, value) in d1)
+    Console.WriteLine($"{key}-{value}");
+
+Console.WriteLine(d1.ContainsKey(4));
+Console.WriteLine(d1.ContainsValue("4"));
+
+// Queue
+
+
+// Set
